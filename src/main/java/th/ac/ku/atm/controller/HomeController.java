30200275821a0356b5fecdbfad2009cf92290711
6110402788 <theerawee.p@ku.th>
2021-01-06 20:21:00 +0700

    first commit

---
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -0,0 +1,18 @@
+package th.ac.ku.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Welcome to Banking Service Website!");
+
+        // return home.html
+        return "home";
+    }
+
+}
---
+++ src/main/java/th/ac/ku/atm/model/BankAccount.java
@@ -0,0 +1,48 @@
+package th.ac.ku.atm.model;
+
+public class BankAccount {
+
+    private int id;
+    private int customerId;
+    private String type;
+    private double balance;
+
+    public BankAccount(int id, int customerId, String type, double balance) {
+        this.id = id;
+        this.customerId = customerId;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
