31aea3d6c72a7607b45aaeae41f14094e411ca97
6110402788 <theerawee.p@ku.th>
2021-01-29 15:32:37 +0700

    Add edit bank account feature

--- src/main/java/th/ac/ku/atm/model/BankAccount.java
+++ src/main/java/th/ac/ku/atm/model/BankAccount.java
@@ -10,13 +10,6 @@ public class BankAccount {
     private String type;
     private double balance;
 
-    public BankAccount(int id, int customerId, String type, double balance) {
-        this.id = id;
-        this.customerId = customerId;
-        this.type = type;
-        this.balance = balance;
-    }
-
     public int getId() {
         return id;
     }
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -42,4 +42,20 @@ public class BankAccountService {
         return Arrays.asList(accounts);
     }
 
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8091/api/bankaccount/" + id;
+
+        ResponseEntity<BankAccount> response =
+                restTemplate.getForEntity(url, BankAccount.class);
+
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" +
+                bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
+
+
 }
